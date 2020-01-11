6c3e87558619762f19db6dd4b33138189d353cbc
Barsoum Edward <barsoumedward1@gmail.com>
2020-01-11 17:23:21 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using PersonalityAnalysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalityAnalysis.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Test()
+        {
+            return View();
+        }
+         
+        [HttpPost]
+        public ActionResult Test(PersonalityQuestion question)
+        {
+            if (ModelState.IsValid)
+            {
+
+                ViewBag.name = question.Name;
+                int sum = question.BestFeeling + question.WalkWay + question.SpeakingWithOther + question.Relaxing + question.SmilingWay +
+                         question.EnteringWay + question.ReactionWay + question.BestColor + question.SleepingWay + question.RepeatDream;
+                return View("Result", sum);
+            }
+            else { return View(); }
+        }
+
+        public ActionResult Haha()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/PersonalityQuestion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalityAnalysis.Models
+{
+    public class PersonalityQuestion
+    {
+        [Required(ErrorMessage ="أدخل أسمك لو سمحت!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int BestFeeling { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int WalkWay { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int SpeakingWithOther { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int Relaxing { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int SmilingWay { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int EnteringWay { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int ReactionWay { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int BestColor { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int SleepingWay { get; set; }
+
+        [Required(ErrorMessage = "أدخل أجابتك لو سمحت!")]
+        public int RepeatDream { get; set; }
+       
+    }
+}
